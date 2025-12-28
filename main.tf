f7f80e4a7e1d7f135ba2b49143938091769aa457
idityaGE <am44910606@gmail.com>
2025-12-28 21:49:18 +0530

    fick ma

--- main.tf
+++ main.tf
@@ -47,3 +47,34 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   ip_protocol       = "-1" # allow all ports
 }
 
+data "aws_ami" "my_ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "ec2_machine" {
+  ami           = data.aws_ami.my_ubuntu.id
+  count         = 1
+  instance_type = "t3.micro"
+
+  security_groups = [aws_security_group.allow_tls.name]
+  key_name        = aws_key_pair.ec2_ssh_key.key_name
+
+  root_block_device {
+    volume_type = "gp3"
+    volume_size = 15 # 15 GB
+  }
+
+  tags = {
+    "name" = "just testing though terraform"
+  }
+}
