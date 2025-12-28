1450f84ac32a9d0bb1a30cc0d456094b1d0f3972
idityaGE <am44910606@gmail.com>
2025-12-28 20:05:13 +0530

    lll

--- main.tf
+++ main.tf
@@ -1,13 +1,49 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.27.0"
+    }
+  }
+}
+
 provider "aws" {
   profile = "default"
   region  = "ap-south-1"
 }
 
-resource "aws_instance" "tf-instance" {
-  ami           = ""
-  instance_type = var.ec2_instance_type
-
-  tags = {
-    "name" = var.instance_name
-  }
+resource "aws_key_pair" "ec2_ssh_key" {
+  key_name   = "ssh-key"
+  public_key = file("terra-key.pub")
 }
+
+resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_default_vpc.default.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # allow all ports
+}
+
